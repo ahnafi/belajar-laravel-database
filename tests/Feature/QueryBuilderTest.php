306476615b53350518523ab377a242d86427c56d
ahnafi <devsulthon@gmail.com>
2025-01-07 20:06:43 +0700

    learn : query builder delete

--- tests/Feature/QueryBuilderTest.php
+++ tests/Feature/QueryBuilderTest.php
@@ -198,4 +198,14 @@ class QueryBuilderTest extends TestCase
 
     }
 
+    function testQueryBuilderDelete()
+    {
+        $this->insertCategories();
+
+        DB::table("categories")->where("id", "=", "SMARTPHONE")->delete();
+        $collection = DB::table("categories")->where("id", "=", "SMARTPHONE")->get();
+
+        self::assertCount(0, $collection);
+    }
+
 }
